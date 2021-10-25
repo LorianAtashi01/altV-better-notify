80021e4b011811c7a3262abc4a09ff7a64b0fb98
Lorian <lorianatashi@icloud.com>
2021-10-25 21:26:43 +0200

    removed environment file

--- src/constants/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  /*
-   *   surface will handle the function: on() & emit()
-   *   if sufrace has the value "AltV" it will set the functions to altV events
-   *   if surface has the value "Web" it will set the functions to normal JS/TS functions
-   *   this handled like that because of not letting the console cry about "alt.on & alt.emit are not functions"
-   */
-  surface: 'Browser', // surface will handle the functions on() & emit() if they should be an altV event or a normal function
-};
